b87b1a4949eaa1aa00bba9c4c6bd8d714506d587
Dummy Krueger <dmy12585@gmail.com>
2021-06-07 17:12:24 +0200

    personal advice implementatio beginned

--- js/dmft.js
+++ js/dmft.js
@@ -89,6 +89,58 @@ context.fill();
 
 
 
+// **************************** PERSONAL INDICATIONS ****************************
+
+var advice1 = "Mastica Gomme allo Xilitolo ed usa Eritritolo come zucchero";
+var advice1text = "Prova a sostituire il classico saccarosio (lo zucchero a cui siamo sempre abituati) con l'Eritritolo (chiedi qualche bustina in reception per provare) e mastica una gomma allo Xilitolo per 4 minuti dopo pranzo se non riesci a lavarti i denti.  Entrambi questi zuccheri non promuovono la carie."
+var advice2 = "Controlla i dolci e la dieta (massimo 5% di zuccheri)";
+var advice2text = "Quanto zucchero ingerisci durante la giornata contribuisce assieme alla frequenza (Snacking) all'insorgenza di nuove carie nella tua bocca. Ricorda che gli zuccheri non devono essere più del 5% del fabbisogno in calorie giornaliero; affidati ad una Dietista se ne hai bisogno.";
+var advice3 = "Controlla la frequenza dei pasti (snacking)";
+var advice3text = "Quanto zucchero mangi è cruciale per l'insorgenza delle carie, ma anche quanto spesso mangi dolci e zuccheri. Limita la loro assunzione ai soli pasti principali. Per approfondire leggi questo post www.recchiadentistibambini.it/nutrizionista/";
+
+
+
+$("#pers-input1").on("change", function () {
+    persIndication("2", $(this).val());
+    $("#pers-input1").val("0");
+});
+
+$("#pers-input2").on("change", function () {
+    persIndication("1", $(this).val());
+    $("#pers-input2").val("0");
+});
+
+
+
+
+function persIndication(num, choice) {
+
+    var pers = "#pers" + num;
+    var persText = "#pers" + num + "text";
+
+    if (choice == "1") {
+        $(pers).css("padding-top", "0")
+        $(pers).text(advice1);
+        $(persText).text(advice1text);
+    } else if (choice == "2") {
+        $(pers).css("padding-top", "0")
+        $(pers).text(advice2);
+        $(persText).text(advice2text);
+    } else if (choice == "3") {
+        $(pers).css("padding-top", "4mm")
+        $(pers).text(advice3);
+        $(persText).text(advice3text);
+    };
+};
+
+
+
+
+
+
+
+
+
 // **************************** DATA TRANSMISSION TO INDEX ****************************
 $("#dataPath-analysis6").attr("href", "../index.html" + queryString + "&prv=no");
 $("#dataPath-dmft6-prv").attr("href", "../index.html" + queryString + "&prv=adul");
